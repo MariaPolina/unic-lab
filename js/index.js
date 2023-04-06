2c3bd206d3b7cc1f57f56cfd85f66f3f2c27e42c
MariaPolina <by.maria.polina@gmail.com>
2023-04-06 21:50:58 +0300

    feat: mobile header and menu ready

--- js/index.js
+++ js/index.js
@@ -1,22 +1,47 @@
 $(document).ready(function () {
 
-    // Line over header submenu
+    if ($(window).width() > 1300) {
+        // Line over header submenu
+        $('.menu__item_with-submenu').on('mouseover', function () {
+            $('.header-line').css('display', 'block');
+            $('.wrapper').addClass('shadow');
+        });
 
-    $('.menu__item_with-submenu').on('mouseover', function () {
-        $('.header-line').css('display', 'block');
-        $('.wrapper').addClass('shadow');
-    });
+        $('.menu__item_with-submenu').on('mouseout', function () {
+            $('.header-line').css('display', 'none');
+            $('.wrapper').removeClass('shadow');
+        });
 
-    $('.menu__item_with-submenu').on('mouseout', function () {
-        $('.header-line').css('display', 'none');
-        $('.wrapper').removeClass('shadow');
-    });
+        // Language select
 
-    // Language select
+        $('.lang__selected').on('click', function () {
+            $('.lang__option').slideToggle(200);
+        });
+    }
 
-    $('.lang__selected').on('click', function () {
-        $('.lang__option').slideToggle(200);
-    });
+    if ($(window).width() < 1301) {
+        $('.menu__item_with-submenu').on('click', function () {
+            $(this).toggleClass('active');
+        });
+
+        $('.header__burger').on('click', function () {
+            $('.header__nav').addClass('active');
+            $('.wrapper').addClass('blur');
+        });
+
+        $('.burger-close').on('click', function () {
+            $('.header__nav').removeClass('active');
+            $('.wrapper').removeClass('blur');
+        });
+
+        $($('.header__menu li').not('.menu__item_with-submenu')).on('click', function () {
+            $('.header__nav').removeClass('active');
+            $('.wrapper').removeClass('blur');
+            $('.menu__item_with-submenu').removeClass('active');
+        });
+    }
+
+
 
 
 });
